3d64eb509410b2cfdacb002f32aa5349d41d2a9a
Justin42 <justin.d.baldwin91@gmail.com>
2014-11-07 23:40:32 -0500

    Update gradle and start scripts. Primarily to avoid long running gradle daemon.

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-until `gradle run > /dev/tty`; do
+createJar() {
+    `eval gradle shadowJar > /dev/tty`
+}
+
+createJar
+runCommand="java -Xmx64m -Xmn64m -jar build/libs/greetbot-1.0-all.jar"
+until `eval ${runCommand} > /dev/tty`; do
         echo "Restarting..."
+        createJar
         sleep 1
 done
