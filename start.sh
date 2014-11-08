4189f1f63a8f4db15ad424b2a1f9666272c1d9c0
Justin42 <justin.d.baldwin91@gmail.com>
2014-11-08 01:59:38 -0500

    Update start script to use gradle wrapper

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 createJar() {
-    `eval gradle shadowJar > /dev/tty`
+    `eval ./gradlew shadowJar > /dev/tty`
 }
 
 createJar
-runCommand="java -Xmx64m -Xmn64m -jar build/libs/greetbot-1.0-all.jar"
+runCommand="java -Xmx64m -Xms64m -jar build/libs/greetbot-1.0-all.jar"
 until `eval ${runCommand} > /dev/tty`; do
         echo "Restarting..."
         createJar
